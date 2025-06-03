88c39ebd30a047121dc01becf5ac7709c67b49af
peterseo9503 <tjehdgus9503@gmail.com>
2025-06-03 12:30:00 +0900

    서버 컴포넌트개념 및 상호작용하는 페이지에 클라이언트 컴포넌트 적용하기

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,7 +1,9 @@
-export default async function Page(
-    {searchParams,}: 
-    {searchParams: Promise<{q: string}>}){
-    const {q} = await searchParams;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
 
-    return <div>Search params : {q}</div>
-}
+  return <div>Search params : {q}</div>;
+} //함수형 컴포넌트의 어떻게 async를 붙일수있냐?
